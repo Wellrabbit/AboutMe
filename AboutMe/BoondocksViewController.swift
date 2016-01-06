b1b23a1c6d8e68183ac489bb229be7e64c5335af
Emily Orton <eort3611@740s-pl-106.local>
2016-01-06 12:22:04 -0700

    Serbs Linked

---
+++ AboutMe/BoondocksViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BoondocksViewController.swift
+//  AboutMe
+//
+//  Created by Orton, Emily on 1/6/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BoondocksViewController : UIViewController
+{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
